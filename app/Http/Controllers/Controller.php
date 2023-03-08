d7afd2ac841d7abf2a488ddf07fe7da7981ed421
Dai Nguyen Trong <dai.nguyentrong@nashtechglobal.com>
2023-03-08 16:38:00 +0700

    Add Swagger UI for project

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,13 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+// Swagger demo
+
+/**
+ * @OA\Info(title="L5 Swagger API", version="0.1")
+ * @OA\Server(url="http://localhost:8000/api")
+ * @OA\PathItem(path="/api")
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
